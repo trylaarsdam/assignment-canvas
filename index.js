67d8cd8370215107006755bccfe6807d3c3d65a6
Todd Rylaarsdam <todd@toddr.org>
2021-02-13 17:14:38 -0600

    auth with database

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
 var express = require('express');
 var app = express();
+const db = require('./src/firestore.js');
 app.listen(3000);
 app.use(express.static('public'))
 
 
 var passport = require('passport');
-var GoogleStrategy = require('passport-google-oauth2').Strategy;
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 passport.use(new GoogleStrategy({
     clientID: '130171068509-trjlgvmuuvs1kdu1g1kg8v5nv8cgv0k0.apps.googleusercontent.com',
     clientSecret: 'XwXE0C6swYLxoxmA7PdkYE17',
     callbackURL: "https://canvas.toddr.org/auth/google/callback",
     passReqToCallback: true
-},
-function(request, accessToken, refreshToken, profile, done){
-    console.log(profile);
-    //passport.user.findOrCreate({googleId: profile.id}, function(err, user){
-    //    return done(err, user);
-    // })
-    return done(null, profile);
-}))
+    }, (accessToken, refreshToken, profile, done) => {
+        await db.getFile('auth','users',{googleID: profile.id}).then((currentUser) =>{
+            if(currentUser){
+                console.log("currentUser "+currentUser);
+            }
+        })
+    }
+))
 
 app.get('/auth/google',
-  passport.authenticate('google', { scope: ['email','profile'] }));
+    passport.authenticate('google', { scope: ['email','profile'] })
+);
 
-app.get('/auth/google/callback', 
-  passport.authenticate('google', { failureRedirect: '/auth/google/failure' }),
-  function(req, res) {
-    res.redirect('/');
-  });
+app.get("/auth/google/redirect", passport.authenticate('google'));
