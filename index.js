6c5a6986f8fb9a1b8503bed7fae912f1868d8065
Todd Rylaarsdam <todd@toddr.org>
2021-02-13 20:00:14 -0600

    added passport session to complete oauth chain

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 var express = require('express');
 var app = express();
+const cookieSession = require('cookie-session');
+const keys = require('./src/keys.js');
 const db = require('./src/firestore.js');
 app.listen(3000);
 app.use(express.static('public'))
 
-
 var passport = require('passport');
 var GoogleStrategy = require('passport-google-oauth20').Strategy;
 
+app.use(cookieSession({
+    maxAge: 24*60*60*1000, //milliseconds in day
+    keys: [keys.session.cookieKey]
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 passport.use(new GoogleStrategy({
-    clientID: '130171068509-trjlgvmuuvs1kdu1g1kg8v5nv8cgv0k0.apps.googleusercontent.com',
-    clientSecret: 'XwXE0C6swYLxoxmA7PdkYE17',
+    clientID: keys.google.clientID,
+    clientSecret: keys.google.clientSecret,
     callbackURL: "https://canvas.toddr.org/auth/google/redirect",
     passReqToCallback: true
     }, async (req, accessToken, refreshToken, profile, done) => {
@@ -37,4 +45,12 @@ app.get('/auth/google',
     passport.authenticate('google', { scope: ['email','profile'] })
 );
 
-app.get("/auth/google/redirect", passport.authenticate('google'));
+app.get("/auth/google/redirect", passport.authenticate('google'),(req,res) => {
+    res.send(req.user);
+    res.send("you reached the redirect URI");
+});
+
+app.get("/auth/logout", (req,res) => {
+    req.logout();
+    res.send(req.user);
+})
