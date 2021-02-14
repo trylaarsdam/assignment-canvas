6b3c207c9e271525b65f57d50623084130256be3
Todd Rylaarsdam <todd@toddr.org>
2021-02-13 20:36:57 -0600

    redir to user profile after login

--- index.js
+++ index.js
@@ -58,17 +58,19 @@ passport.deserializeUser( async (id, done) => {
 })
 
 app.get('/auth/google',
-    passport.authenticate('google', { scope: ['email','profile'] }), (req,res) => {
-        res.redirect("/templates/user.html")
-    }
+    passport.authenticate('google', { scope: ['email','profile'] })
 );
 
 app.get("/auth/google/redirect", passport.authenticate('google'),(req,res) => {
-    res.send(req.user);
-    res.send("you reached the redirect URI");
+    res.redirect('/user/' + req.user.googleID);
+    //res.send("you reached the redirect URI");
 });
 
 app.get("/auth/logout", (req,res) => {
     req.logout();
     res.send(req.user);
+})
+
+app.get("/user/:username", passport.authenticate('google'), (req,res) => {
+    res.send('username profile';
 })
