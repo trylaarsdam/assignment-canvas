844bfca2da1a3a4f115103fabc95a397693d1647
Todd Rylaarsdam <todd@toddr.org>
2021-02-13 22:04:14 -0600

    !== instead of ==

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ app.get("/auth/logout", (req,res) => {
 })
 
 app.get("/user", (req,res) => {
-    if(typeof(req.user) == 'undefined'){
+    if(typeof(req.user) !== 'undefined'){
         console.log("googleID: " + req.user.googleID);
         res.send(req.user);
     }
