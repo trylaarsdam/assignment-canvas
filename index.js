5037fdf2b456434f960fede1d32e10b585504628
Todd Rylaarsdam <todd@toddr.org>
2021-02-13 19:05:25 -0600

    changed done to include id instead of undefined variable

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ passport.use(new GoogleStrategy({
             else{
                 console.log("creating new user");
                 await db.insertFile('auth','users', {googleID: profile.id, name: profile.displayName, profilePicture: profile.photos[0].value, email: profile.emails[0].value}).then(async (id) => {
-                    done(null, newUser);
+                    done(null, id);
                 })
             }
         })
