3adf1a24a13fb9bbb5000c56e831e8462d522444
Todd Rylaarsdam <todd@toddr.org>
2021-02-14 19:40:27 -0600

    updated request

--- public/js/navigation.js
+++ public/js/navigation.js
@@ -5,9 +5,10 @@ window.onload = function() {
 
 function setCanvasAPI(googleID){
   var xhttp = new XMLHttpRequest();
-  xhttp.open("POST", "/api/setCanvasAPI", true);
   console.log(googleID);
   console.log(inputField.innerText);
-  xhttp.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
-  xhttp.send({google: googleID, canvas: inputField.innerText})
+  fetch("/api/setCanvasAPI", {
+    method: "POST",
+    body: JSON.stringify({google: googleID, canvas: inputField.value})
+  })
 }
