5e4fa2658797b011f5b1e49a18a19b0f23350e23
Todd Rylaarsdam <todd@toddr.org>
2021-02-14 19:19:19 -0600

    changed submit

--- public/js/navigation.js
+++ public/js/navigation.js
@@ -3,5 +3,7 @@ const inputField = document.getElementById('canvasAPI');
 function setCanvasAPI(googleID){
   var xhttp = new XMLHttpRequest();
   xhttp.open("POST", "/api/setCanvasAPI", true);
+  console.log(googleID);
+  console.log(inputField);
   xhttp.send(JSON.stringify({google: googleID, canvas: inputField}));
 }
