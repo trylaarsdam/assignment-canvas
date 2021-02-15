61ecf86f9d63c5294f72c145ff2073473ff4fd2a
Todd Rylaarsdam <todd@toddr.org>
2021-02-14 19:52:32 -0600

    header

--- index.js
+++ index.js
@@ -103,13 +103,13 @@ app.get("/user", (req,res) => {
 
 app.post("/api/setCanvasAPI", async (req,res) => {
     console.log('REQUEST BODY '+req.body);
-    console.log("google id from api " + req.google)
+    console.log("google id from api " + req.body.google)
     if(typeof(req.body.google) != "undefined"){
-        await db.getFile('auth', 'users', {googleID: req.google}).then(async (dbFile) => {
+        await db.getFile('auth', 'users', {googleID: req.body.google}).then(async (dbFile) => {
             console.log("dbFile + " + dbFile[0])
             if(dbFile.length() == 1){
                 console.log("dbFile length was 1")
-                dbFile[0].canvasKey = req.canvas;
+                dbFile[0].canvasKey = req.body.canvas;
                 await db.updateFile('auth', 'users', dbFile[0], dbFile[0].id)
                 return res.send({status: "updated"})
             }
--- public/js/navigation.js
+++ public/js/navigation.js
@@ -4,11 +4,14 @@ window.onload = function() {
 }
 
 function setCanvasAPI(googleID){
-  var xhttp = new XMLHttpRequest();
+  //var xhttp = new XMLHttpRequest();
   console.log(googleID);
   console.log(inputField.innerText);
   fetch("/api/setCanvasAPI", {
     method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
     body: JSON.stringify({google: googleID, canvas: "inputField.value"})
   })
 }
