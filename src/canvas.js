4ecea10d38c915e77b40cf28f0b15b5878716970
Todd Rylaarsdam <todd@toddr.org>
2021-02-15 23:08:33 -0600

    async

--- src/canvas.js
+++ src/canvas.js
@@ -1,17 +1,25 @@
-module.exports = {getClasses}
+module.exports = {getClasses, getAnnouncements, getAssignments}
 const request = require('request');
 const fetch = require('node-fetch')
 const baseURL = 'https://timothy.instructure.com/api/v1/'
 
-function getClasses(api) {
+async function getClasses(api) {
     if(api != null){
         console.log("canvas.js - api key not null")
         var apiResponse;
-        return fetch(baseURL + "courses", {
+        return await fetch(baseURL + "courses", {
             method: 'GET',
             headers: {
                 'Authorization': 'Bearer ' + api
             }
         })
     }
+}
+
+function getAssignments(api, classID) {
+
+}
+
+function getAnnouncements(api, classID) {
+
 }
