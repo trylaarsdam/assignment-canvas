c84be5b44cf34dc095dd878aafb96f3822adbc53
Todd Rylaarsdam <todd@toddr.org>
2021-02-15 22:59:44 -0600

    switch to fetch for canvas api call

--- src/canvas.js
+++ src/canvas.js
@@ -5,19 +5,11 @@ const baseURL = 'https://timothy.instructure.com/api/v1/'
 function getClasses(api) {
     if(api != null){
         console.log("canvas.js - api key not null")
-        request({
-            url: baseURL + 'courses',
+        var apiResponse;
+        return fetch(baseURL + "courses", {
+            method: 'GET',
             headers: {
                 'Authorization': 'Bearer ' + api
-            }
-        },
-        function(err,res) {
-            if(err) {
-                console.error(err);
-            }
-            else {
-                console.log("canvas.js - " + res.body)
-                return res.body
             }
         })
     }
