b071d8929b96a9b0f5b78b2647a0a2b3bee46eb6
Todd Rylaarsdam <50586094+trylaarsdam@users.noreply.github.com>
2021-02-25 20:29:10 -0600

    added ,

--- src/canvas.js
+++ src/canvas.js
@@ -22,7 +22,7 @@ async function getClasses(api) {
 async function getAnnouncements(api, classID, formattedDate) {
     if(api != null){
         console.log("canvas.js - api key not null")
-        return fetch(baseURL + "announcements?context_codes[]=course_" + classID + '&start_date=2018-01-01&end_date=', + formattedDate{
+        return fetch(baseURL + "announcements?context_codes[]=course_" + classID + '&start_date=2018-01-01&end_date=', + formattedDate, {
             withCredentials: true,
             headers: {
                 'Authorization': 'Bearer ' + api
