dc15df06742fd85d534bf99d57dc3ac191b0c352
Todd Rylaarsdam <50586094+trylaarsdam@users.noreply.github.com>
2021-03-06 17:34:58 -0600

    fetch fix

--- public/js/feed.js
+++ public/js/feed.js
@@ -4,6 +4,8 @@ async function loadData(canvasKey) {
     fetch("https://canvas.toddr.org/api/html/feed/" + canvasKey, {
         method: "GET",
     }).then(data => {
+        data.text()
+    }).then(text => {
         document.body.innerHTML = document.body.innerHTML + data.toString()
     }).then(
         document.getElementById("loader").style.display = 'none'
