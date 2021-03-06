45ef2744c8d34b788fef6d0e683e18763f30456f
Todd Rylaarsdam <50586094+trylaarsdam@users.noreply.github.com>
2021-03-06 17:45:56 -0600

    await

--- public/js/feed.js
+++ public/js/feed.js
@@ -4,10 +4,10 @@ async function loadData(canvasKey) {
     await fetch("https://canvas.toddr.org/api/html/feed/" + canvasKey, {
         method: "GET",
     }).then(data => {
-        return data.text()
+        return await data.text()
     }).then(text => {
         document.body.innerHTML = document.body.innerHTML + text
     }).then(
         document.getElementById("loader").style.display = 'none'
     )
-} 
+}
