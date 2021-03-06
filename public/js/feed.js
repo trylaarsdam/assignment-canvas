493eb32774f9ad3d6aa967af28e2e329f297923d
Todd Rylaarsdam <50586094+trylaarsdam@users.noreply.github.com>
2021-03-06 17:50:47 -0600

    undo

--- public/js/feed.js
+++ public/js/feed.js
@@ -1,10 +1,10 @@
 console.log('placeholder')
 
-async function loadData(canvasKey) {
-    await fetch("https://canvas.toddr.org/api/html/feed/" + canvasKey, {
+function loadData(canvasKey) {
+    fetch("https://canvas.toddr.org/api/html/feed/" + canvasKey, {
         method: "GET",
     }).then(async data => {
-        return await data.text()
+        return data.text()
     }).then(text => {
         document.body.innerHTML = document.body.innerHTML + text
     }).then(
