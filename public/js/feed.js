22a23f5845a077003f5c598291b42d5cd26665dc
Todd Rylaarsdam <50586094+trylaarsdam@users.noreply.github.com>
2021-03-06 17:38:26 -0600

    return

--- public/js/feed.js
+++ public/js/feed.js
@@ -1,10 +1,10 @@
 console.log('placeholder')
 
-async function loadData(canvasKey) {
+function loadData(canvasKey) {
     fetch("https://canvas.toddr.org/api/html/feed/" + canvasKey, {
         method: "GET",
     }).then(data => {
-        data.text()
+        return data.text()
     }).then(text => {
         document.body.innerHTML = document.body.innerHTML + text
     }).then(
