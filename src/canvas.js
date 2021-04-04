dd44355b28bfbbb0a8463c326401526df147570d
Todd Rylaarsdam <50586094+trylaarsdam@users.noreply.github.com>
2021-04-03 22:10:14 -0500

    prettier

--- src/canvas.js
+++ src/canvas.js
@@ -1,101 +1,101 @@
-module.exports = {getClasses, getAnnouncements, getAssignments, getFeedAnnouncements, getAnnouncementReplies, getCourseName}
+module.exports = { getClasses, getAnnouncements, getAssignments, getFeedAnnouncements, getAnnouncementReplies, getCourseName }
 const baseURL = 'https://timothy.instructure.com/api/v1/'
 const fetch = require('node-fetch');
 const options = {
-    hostname: 'timothy.instructure.com',
-    port: 443,
-    method: 'GET'
+  hostname: 'timothy.instructure.com',
+  port: 443,
+  method: 'GET'
 }
 
 async function getClasses(api) {
-    if(api != null){
-        console.log("canvas.js - api key not null")
-        return fetch(baseURL + "courses", {
-            withCredentials: true,
-            headers: {
-                'Authorization': 'Bearer ' + api
-            }
-        })
-    }
+  if (api != null) {
+    console.log("canvas.js - api key not null")
+    return fetch(baseURL + "courses", {
+      withCredentials: true,
+      headers: {
+        'Authorization': 'Bearer ' + api
+      }
+    })
+  }
 }
 
-async function getCourseName(api, classID){
-    if(api != null){
-        console.log("canvas.js - getCourseName")
-        return fetch(baseURL + "courses/" + classID, {
-            withCredentials: true,
-            headers: {
-                'Authorization': 'Bearer ' + api
-            }
-        })
-    }
+async function getCourseName(api, classID) {
+  if (api != null) {
+    console.log("canvas.js - getCourseName")
+    return fetch(baseURL + "courses/" + classID, {
+      withCredentials: true,
+      headers: {
+        'Authorization': 'Bearer ' + api
+      }
+    })
+  }
 }
 
 async function getAnnouncements(api, classID) {
-    if(api != null){
-        var today = new Date();
-        var dd = String(today.getDate()).padStart(2, '0');
-        var mm = String(today.getMonth() + 1).padStart(2, '0');
-        var yyyy = today.getFullYear();
-        var formattedDate = yyyy + '-' + mm + '-' + dd
-        console.log("canvas.js - api key not null")
-        return fetch(baseURL + "announcements?context_codes[]=course_" + classID + '&start_date=2018-01-01&end_date=' + formattedDate, {
-            withCredentials: true,
-            headers: {
-                'Authorization': 'Bearer ' + api
-            }
-        })
-    }
+  if (api != null) {
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0');
+    var yyyy = today.getFullYear();
+    var formattedDate = yyyy + '-' + mm + '-' + dd
+    console.log("canvas.js - api key not null")
+    return fetch(baseURL + "announcements?context_codes[]=course_" + classID + '&start_date=2018-01-01&end_date=' + formattedDate, {
+      withCredentials: true,
+      headers: {
+        'Authorization': 'Bearer ' + api
+      }
+    })
+  }
 }
 
-async function getAnnouncementReplies(api, classID, announcementID){
-    if(api != null) {
-        console.log("canvas.js - getAnnouncementReplies api key not null");
-        return fetch(baseURL + "courses/" + classID + "/discussion_topics/" + announcementID + "/view", {
-            withCredentials: true,
-            headers: {
-                'Authorization': 'Bearer ' + api
-            }
-        })
-    }
+async function getAnnouncementReplies(api, classID, announcementID) {
+  if (api != null) {
+    console.log("canvas.js - getAnnouncementReplies api key not null");
+    return fetch(baseURL + "courses/" + classID + "/discussion_topics/" + announcementID + "/view", {
+      withCredentials: true,
+      headers: {
+        'Authorization': 'Bearer ' + api
+      }
+    })
+  }
 }
 
 async function getFeedAnnouncements(api, courseList) {
-    if(api != null){
-        var today = new Date();
-        var dd = String(today.getDate()).padStart(2, '0');
-        var mm = String(today.getMonth() + 1).padStart(2, '0');
-        var yyyy = today.getFullYear();
-        var formattedDate = yyyy + '-' + mm + '-' + dd
-        var courseIDs = [];
-        var courseString = "";
-        if(courseList != null){
-            console.log(courseList);
-            console.log(courseList.length);
-            for(var course = 0; course < courseList.length; course++){
-                console.log(courseList.length);
-                //console.log(course);
-                console.log(courseList[course].id)
-                courseIDs[course] = courseList[course].id;
-            }
-            console.log("canvas.js - api key not null, courseList provided")
-            console.log(courseIDs);
-            for(var id = 0; id < courseIDs.length; id++){
-                courseString += "context_codes[]=course_" + courseIDs[id] + "&"
-            }
-            console.log(courseString);
-            return fetch(baseURL + "announcements?per_page=25&" + courseString + 'start_date=2018-01-01&end_date=' + formattedDate, {
-                withCredentials: true,
-                headers: {
-                    'Authorization': 'Bearer ' + api
-                }
-            })
+  if (api != null) {
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0');
+    var yyyy = today.getFullYear();
+    var formattedDate = yyyy + '-' + mm + '-' + dd
+    var courseIDs = [];
+    var courseString = "";
+    if (courseList != null) {
+      console.log(courseList);
+      console.log(courseList.length);
+      for (var course = 0; course < courseList.length; course++) {
+        console.log(courseList.length);
+        //console.log(course);
+        console.log(courseList[course].id)
+        courseIDs[course] = courseList[course].id;
+      }
+      console.log("canvas.js - api key not null, courseList provided")
+      console.log(courseIDs);
+      for (var id = 0; id < courseIDs.length; id++) {
+        courseString += "context_codes[]=course_" + courseIDs[id] + "&"
+      }
+      console.log(courseString);
+      return fetch(baseURL + "announcements?per_page=25&" + courseString + 'start_date=2018-01-01&end_date=' + formattedDate, {
+        withCredentials: true,
+        headers: {
+          'Authorization': 'Bearer ' + api
         }
-        else{
-            return {error: "courseList not provided"}
-        }
-        
+      })
     }
+    else {
+      return { error: "courseList not provided" }
+    }
+
+  }
 }
 
 function getAssignments(api, classID) {
