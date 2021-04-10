79dd652abcbc7a1968b6e159f0a766a7ef9e3d22
Todd Rylaarsdam <50586094+trylaarsdam@users.noreply.github.com>
2021-04-09 22:34:25 -0500

    button color changes

--- public/js/feed.js
+++ public/js/feed.js
@@ -17,30 +17,38 @@ function updateBody(text) {
   return document.body.innerHTML = document.body.innerHTML + text
 }
 
-function announcements() {
+function announcements(button) {
   var elements = document.getElementsByClassName("container announcement");
   if (announcementToggle) {
     for (var i = 0; i < elements.length; i++) {
       elements[i].style.display = "none";
     }
+    announcementToggle = false;
+    button.style.background = '#555555';
   }
   else {
     for (var i = 0; i < elements.length; i++) {
       elements[i].style.display = ""
     }
+    announcementToggle = true;
+    button.style.background = '#ff0000';
   }
 }
 
-function assignments() {
+function assignments(button) {
   var elements = document.getElementsByClassName("container assignment");
   if (assignmentToggle) {
     for (var i = 0; i < elements.length; i++) {
       elements[i].style.display = "none";
     }
+    button.style.background = '#555555';
+    assignemntToggle = false;
   }
   else {
     for (var i = 0; i < elements.length; i++) {
-      elements[i].style.display = "block"
+      elements[i].style.display = ""
     }
+    assignmentToggle = true;
+    button.style.background = '#ff0000';
   }
 }
