a4eefe600dcc09a731eed4d6c3f0d51f0531398e
Todd Rylaarsdam <50586094+trylaarsdam@users.noreply.github.com>
2021-04-09 22:07:40 -0500

    toggle announcments

--- public/js/feed.js
+++ public/js/feed.js
@@ -1,15 +1,46 @@
+var announcementToggle = true;
+var assignmentToggle = true;
+
 function loadData(canvasKey) {
-    fetch("https://canvas.toddr.org/api/html/feed/" + canvasKey, {
-        method: "GET",
-    }).then(async data => {
-        return data.text()
-    }).then(text => {
-        updateBody(text).then(
-            document.getElementById("loader").style.display = 'none'
-        )
-    })
+  fetch("https://canvas.toddr.org/api/html/feed/" + canvasKey, {
+    method: "GET",
+  }).then(async data => {
+    return data.text()
+  }).then(text => {
+    updateBody(text).then(
+      document.getElementById("loader").style.display = 'none'
+    )
+  })
 }
 
-function updateBody(text){
-    return document.body.innerHTML = document.body.innerHTML + text
+function updateBody(text) {
+  return document.body.innerHTML = document.body.innerHTML + text
+}
+
+function announcements() {
+  var elements = document.getElementsByClassName("announcment");
+  if (announcementToggle) {
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].display = "none";
+    }
+  }
+  else {
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].display = "block"
+    }
+  }
+}
+
+function assignments() {
+  var elements = document.getElementsByClassName("assignment");
+  if (assignmentToggle) {
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].display = "none";
+    }
+  }
+  else {
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].display = "block"
+    }
+  }
 }
