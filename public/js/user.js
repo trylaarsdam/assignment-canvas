5ebfd027df43eee51cf854d211a1d562e754f6b0
Todd Rylaarsdam <50586094+trylaarsdam@users.noreply.github.com>
2021-04-17 14:04:56 -0500

    onload

--- public/js/user.js
+++ public/js/user.js
@@ -9,6 +9,18 @@ window.onload = function () {
   var modal = document.getElementById("myModal");
 
   var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
 }
 
 async function setCanvasAPI(databaseUUID) {
@@ -51,14 +63,3 @@ async function setCanvasURL(databaseUUID) {
 }
 
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
