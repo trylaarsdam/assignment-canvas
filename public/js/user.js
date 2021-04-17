d27e0967700de211431dfc2466284515544600f4
Todd Rylaarsdam <50586094+trylaarsdam@users.noreply.github.com>
2021-04-17 13:36:35 -0500

    format canvas url string

--- public/js/user.js
+++ public/js/user.js
@@ -1,12 +1,12 @@
 var inputFieldAPI;
 var inputFieldURL;
 
-window.onload = function() {
+window.onload = function () {
   inputFieldAPI = document.getElementById('canvasAPI');
   inputFieldURL = document.getElementById('canvasURL')
 }
 
-async function setCanvasAPI(databaseUUID){
+async function setCanvasAPI(databaseUUID) {
   //var xhttp = new XMLHttpRequest();
   console.log(databaseUUID);
   console.log(inputFieldAPI.value);
@@ -15,19 +15,31 @@ async function setCanvasAPI(databaseUUID){
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded'
     },
-    body: JSON.stringify({uuid: databaseUUID, canvas: "inputFieldAPI.value"})
+    body: JSON.stringify({ uuid: databaseUUID, canvas: "inputFieldAPI.value" })
   })
 }
 
-async function setCanvasURL(databaseUUID){
+async function setCanvasURL(databaseUUID) {
   //var xhttp = new XMLHttpRequest();
   console.log(databaseUUID);
   console.log(inputFieldURL.value);
-  await fetch("https://canvas.toddr.org/api/setCanvasURL/" + databaseUUID + "/" + inputFieldURL.value.toString(), {
+  var httpRemoved;
+  if (inputFieldURL.value.startsWith('https://') || inputFieldURL.value.startsWith('http://')) {
+    httpRemoved = inputFieldURL.value.split("://")[1];
+  }
+  else {
+    httpRemoved = inputFieldURL.value
+  }
+
+  if (httpRemoved.split("/").length > 1) {
+    httpRemoved = httpRemoved.split("/")[0];
+    //TODO add modal
+  }
+  await fetch("https://canvas.toddr.org/api/setCanvasURL/" + databaseUUID + "/" + httpRemoved, {
     method: "POST",
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded'
     },
-    body: JSON.stringify({uuid: databaseUUID, canvas: "inputFieldURL.value"})
+    body: JSON.stringify({ uuid: databaseUUID, canvas: httpRemoved })
   })
 }
