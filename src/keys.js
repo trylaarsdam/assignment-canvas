3051afd79255be1789fce146952a2fb21b126f56
Todd Rylaarsdam <50586094+trylaarsdam@users.noreply.github.com>
2021-07-03 17:40:40 -0500

    beginning of statuspage update

--- src/keys.js
+++ src/keys.js
@@ -1,9 +1,13 @@
 module.exports = {
-    google:{
-        clientID:"130171068509-trjlgvmuuvs1kdu1g1kg8v5nv8cgv0k0.apps.googleusercontent.com",
-        clientSecret:"XwXE0C6swYLxoxmA7PdkYE17",
-    },
-    session:{
-        cookieKey: "b5bed0569cd436f7e0ea3b7c62c9735309c2dc3f690db84313e2907d2b335cb1048683387302b77ad3a1040067ca954382def2230cc09da1b6d4a322c52c5e25"
-    }
+  google: {
+    clientID: "130171068509-trjlgvmuuvs1kdu1g1kg8v5nv8cgv0k0.apps.googleusercontent.com",
+    clientSecret: "XwXE0C6swYLxoxmA7PdkYE17",
+  },
+  session: {
+    cookieKey: "b5bed0569cd436f7e0ea3b7c62c9735309c2dc3f690db84313e2907d2b335cb1048683387302b77ad3a1040067ca954382def2230cc09da1b6d4a322c52c5e25"
+  },
+  atlassian: {
+    orgID: "3b4kj57d-092a-1k7d-7j4j-j1a20b2b11kb",
+    key: "rALSE2TjLqG6ck7rVLts"
+  }
 }
---
+++ src/status.js
@@ -0,0 +1,23 @@
+module.exports = {}
+const keys = require('./keys.js');
+const apikey = keys.atlassian.orgID;
+
+async function setOperational() {
+
+}
+
+async function setDegraded() {
+
+}
+
+async function setMajorOutage() {
+
+}
+
+async function setMinorOutage() {
+
+}
+
+async function setMaintenance() {
+
+}
