427b84315382071be2a256c70b5e478571582c8e
Todd Rylaarsdam <todd@toddr.org>
2022-05-15 15:23:01 -0500

    course page

--- src/views/CourseView.vue
+++ src/views/CourseView.vue
@@ -1,5 +1,14 @@
 <template>
-  <div></div>
+  <div class="courseContainer" v-if="!loading">
+    <h1>{{ this.course.name }}</h1>
+  </div>
+  <div class="loading" v-else>
+    <v-progress-circular
+      :size="50"
+      color="primary"
+      indeterminate
+    ></v-progress-circular>
+  </div>
 </template>
 
 <script>
@@ -7,18 +16,22 @@ const axios = require("axios").default;
 
 export default {
   name: "CourseView",
-  props: ["id"],
   data() {
     return {
+      loading: true,
+      course: {
+        name: "Unknown Course",
+      },
       assignments: [],
       announcements: [],
     };
   },
   async created() {
     // console.log("Setting breadcrumbs from announcements");
+    console.log("course id", this.id);
     try {
       const response = await axios.get(
-        "http://10.128.1.199:7001/api/courses/assignments/" + this.id,
+        "http://10.128.1.199:7001/api/courses/" + this.$route.params.id,
         {
           auth: {
             username: "trylaarsdam",
@@ -27,7 +40,24 @@ export default {
         }
       );
       console.log("Response: ", response.data.data);
-      this.courses = response.data.data;
+      this.course = response.data.data;
+    } catch (error) {
+      console.error(error);
+    }
+    try {
+      const response = await axios.get(
+        "http://10.128.1.199:7001/api/courses/" +
+          this.$route.params.id +
+          "/assignments",
+        {
+          auth: {
+            username: "trylaarsdam",
+            password: "test12345",
+          },
+        }
+      );
+      console.log("Response: ", response.data.data);
+      this.assignments = response.data.data;
     } catch (error) {
       console.error(error);
     }
@@ -41,12 +71,23 @@ export default {
         // to: "/announcements",
       },
       {
-        text: "Course",
-        disabled: true,
-        href: "/course/" + this.id,
+        text: this.course.name,
+        disabled: false,
+        href: "/course/" + this.$route.params.id,
         exact: true,
       },
     ]);
+    this.loading = false;
   },
 };
 </script>
+
+<style local>
+.loading {
+  margin: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
